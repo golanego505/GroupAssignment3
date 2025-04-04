72d3eac3bb1977099671753dbdd0ba1ee252f2c0
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-03 23:34:01 -0500

    Edited comments.

--- src/mylittlemozart/TestClass.java
+++ src/mylittlemozart/TestClass.java
@@ -12,13 +12,14 @@ public class TestClass {
 	 * Method tests if the midi Csv file has all valid lines and the # of objects in the list matches the # of lines in the file
 	 * Checking for proper file parsing.
 	 * 
+	 * 
 	 * @return true if all lines in the file are valid and # of objects matches # of lines, false if not or file cannot be read.
 	 */
 	public static boolean testParse()
 	{
 		try 
 		{
-			List<MidiEventData> events = MidiCsvParser.parseCsv("mystery_song.csv");
+			List<MidiEventData> events = MidiCsvParser.parseCsv("src/files/mystery_song.csv");
 			if(events.size() == MidiCsvParser.lineCount)
 			{
 				//Midi objects match each note in the file
