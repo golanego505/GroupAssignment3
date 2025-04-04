e1ee4ba6e232238a76cdc7f01a3ce30a5298a5ec
Lane Flint <lane.blair.flint@gmail.com>
2025-04-03 22:52:16 -0500

    Created LegatoMidiEventFactory.java

---
+++ src/mylittlemozart/factory/LegatoMidiEventFactory.java
@@ -0,0 +1,20 @@
+package mylittlemozart.factory;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+
+public class LegatoMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel, int duration) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
