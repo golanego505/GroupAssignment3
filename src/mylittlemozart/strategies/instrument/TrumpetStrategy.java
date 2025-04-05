6d8cb72cf076d065855fadfbd49e0b27e3299ff4
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-05 16:21:19 -0500

    Implemented instrument strategy, strategy changes instrument on track to trumpet.

--- src/mylittlemozart/strategies/instrument/TrumpetStrategy.java
+++ src/mylittlemozart/strategies/instrument/TrumpetStrategy.java
@@ -1,5 +1,20 @@
-package mylittlemozart.strategies;
+package mylittlemozart.strategies.instrument;
 
-public class TrumpetStrategy {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class TrumpetStrategy implements InstrumentStrategy{
+	@Override
+	public void applyInstrument(Track track, int channel) {
+		//Trumpet is instrument # 56
+		// setMessage(commandType, channel, data1 (e.g. note or instrument), data2 (e.g. velocity or 0))
+		try {
+			ShortMessage message = new ShortMessage();
+			message.setMessage(ShortMessage.PROGRAM_CHANGE, channel, 56, 0);
+		} catch (InvalidMidiDataException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
