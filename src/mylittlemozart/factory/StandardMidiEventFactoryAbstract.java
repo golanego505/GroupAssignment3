8e09470ce219d5ffa97002b89ed809897f63ebef
Lane Flint <lane.blair.flint@gmail.com>
2025-04-05 15:50:51 -0500

    Created StandardMidiEventFactoryAbstract.java, override method from interface to instantiate class

---
+++ src/mylittlemozart/factory/StandardMidiEventFactoryAbstract.java
@@ -0,0 +1,9 @@
+package mylittlemozart.factory;
+
+public class StandardMidiEventFactoryAbstract implements MidiEventFactoryAbstract {
+
+	@Override
+	public MidiEventFactory createFactory() {
+		 return new StandardMidiEventFactory();
+	}
+}
