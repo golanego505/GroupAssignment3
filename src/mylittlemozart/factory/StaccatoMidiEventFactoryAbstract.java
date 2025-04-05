e2720b6c7585f23014f8e7653514a1bcfd4912fd
Lane Flint <lane.blair.flint@gmail.com>
2025-04-05 16:06:58 -0500

    Added StaccatoMidiEventFactoryAbstract.java, override method from interface

---
+++ src/mylittlemozart/factory/StaccatoMidiEventFactoryAbstract.java
@@ -0,0 +1,10 @@
+package mylittlemozart.factory;
+
+public class StaccatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract {
+
+	@Override
+	public MidiEventFactory createFactory() {
+		return new StaccatoMidiEventFactory();
+	}
+
+}
