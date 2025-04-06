a2a74715a93b22b54dd80168583164b44f09a15d
Lane Flint <lane.blair.flint@gmail.com>
2025-04-05 19:55:14 -0500

    Adding JavaDocs for classes and methods

--- src/mylittlemozart/factory/LegatoMidiEventFactoryAbstract.java
+++ src/mylittlemozart/factory/LegatoMidiEventFactoryAbstract.java
@@ -1,7 +1,13 @@
 package mylittlemozart.factory;
 
+/**
+ * {@inheritDoc}
+ */
 public class LegatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public MidiEventFactory createFactory() {
 		return new LegatoMidiEventFactory();
