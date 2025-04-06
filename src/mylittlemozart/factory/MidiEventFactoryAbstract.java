fb1cb536c2d8f8e8dc6aa390daa5a7cf0f6205e1
Lane Flint <lane.blair.flint@gmail.com>
2025-04-05 19:55:38 -0500

    Adding JavaDocs for classes and methods

--- src/mylittlemozart/factory/MidiEventFactoryAbstract.java
+++ src/mylittlemozart/factory/MidiEventFactoryAbstract.java
@@ -1,7 +1,14 @@
 package mylittlemozart.factory;
 
+/**
+ * Abstract factory interface for creating MidiEventFactory objects
+ * Returns Standard, Staccato, and Legato MidiEventFactory objects
+ */
 public interface MidiEventFactoryAbstract {
-	
+	/**
+	 * Creates and returns instance of MidiEventFactory objects
+	 * @return		MidiEventFactoryObjects
+	 */
 	 MidiEventFactory createFactory();
 	 
 }
