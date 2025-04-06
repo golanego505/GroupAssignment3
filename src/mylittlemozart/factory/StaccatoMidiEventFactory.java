b250738d38dbc4003ce90bc4591204e6bb33a467
Lane Flint <lane.blair.flint@gmail.com>
2025-04-05 19:56:28 -0500

    Adding JavaDocs for classes and methods

--- src/mylittlemozart/factory/StaccatoMidiEventFactory.java
+++ src/mylittlemozart/factory/StaccatoMidiEventFactory.java
@@ -4,8 +4,16 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 
+
+/**
+ * Factory class that produces staccato note events
+ * Achieved by subtracting 120 ticks to the duration of a note
+ */
 public class StaccatoMidiEventFactory implements MidiEventFactory {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
 		ShortMessage message = new ShortMessage();
@@ -13,6 +21,9 @@ public class StaccatoMidiEventFactory implements MidiEventFactory {
 	    return new MidiEvent(message, tick);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
 		 ShortMessage message = new ShortMessage();
