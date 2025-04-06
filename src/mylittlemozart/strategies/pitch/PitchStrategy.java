510bf8eeae75c72789c92f6cee3059142bc0ae62
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-05 19:32:34 -0500

    Added Java docs to pitch strategies.

--- src/mylittlemozart/strategies/pitch/PitchStrategy.java
+++ src/mylittlemozart/strategies/pitch/PitchStrategy.java
@@ -1,7 +1,16 @@
 package mylittlemozart.strategies.pitch;
 
+/**
+ * Strategy for applying a pitch change
+ */
 public interface PitchStrategy {
 	
+	/**
+	 * Applies a change of pitch to a given note from a MidiEvent.
+	 * 
+	 * @param note The original MIDI note number.
+	 * @return The note number after being modified.
+	 */
 	int modifyPitch(int note);
 
 }
