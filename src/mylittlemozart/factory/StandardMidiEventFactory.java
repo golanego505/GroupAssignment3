b580e59100ede5bb786179480979fbc19bb4ea16
Lane Flint <lane.blair.flint@gmail.com>
2025-04-05 19:57:09 -0500

    Adding JavaDocs for classes and methods

--- src/mylittlemozart/factory/StandardMidiEventFactory.java
+++ src/mylittlemozart/factory/StandardMidiEventFactory.java
@@ -4,8 +4,15 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 
+/**
+ * Factory class that produces standard note events
+ * Uses default values given from CSV for timing and duration of notes
+ */
 public class StandardMidiEventFactory implements MidiEventFactory {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
 		 ShortMessage message = new ShortMessage();
@@ -13,6 +20,9 @@ public class StandardMidiEventFactory implements MidiEventFactory {
 		 return new MidiEvent(message, tick);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
 		ShortMessage message = new ShortMessage();
